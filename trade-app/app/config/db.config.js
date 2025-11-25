889f4e7848316fe356125e0a83de66d569003d8c
Denrantengeiten <aagaitus@gmail.com>
2025-11-25 03:31:12 +0300

    controller and routes added1

--- trade-app/app/config/db.config.js
+++ trade-app/app/config/db.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-HOST: process.env.DB_HOST,
-USER: process.env.DB_USER,
-PASSWORD: process.env.DB_PASSWORD,
-DB: process.env.DB_NAME,
-port: process.env.DB_PORT,
-dialect: "postgres",
-pool: {
-max: 5,
-min: 0,
-acquire: 30000,
-idle: 10000
-}
+  HOST: "postgresdb",
+  USER: "user",
+  PASSWORD: "password",
+  DB: "hotel_db",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
